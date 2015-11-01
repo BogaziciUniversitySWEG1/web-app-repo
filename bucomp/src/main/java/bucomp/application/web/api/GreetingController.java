b360f713cc15c8fea8273cb65ec778763237ce49
Alper Karatepe <alperkaratepe@gmail.com>
2015-11-01 13:28:21 +0200

    Added new functionalities to sample code

---
+++ bucomp/src/main/java/bucomp/application/web/api/GreetingController.java
@@ -0,0 +1,77 @@
+package bucomp.application.web.api;
+
+import java.math.BigInteger;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import bucomp.application.model.Greeting;
+
+@RestController
+public class GreetingController {
+
+	private static BigInteger nextId;
+	private static Map<BigInteger, Greeting> greetingMap;
+	
+	static {
+		Greeting g1 = new Greeting();
+		g1.setText("Hello World");
+		save(g1);
+		
+		Greeting g2 = new Greeting();
+		g2.setText("Hola Mundo");
+		save(g2);
+	}
+	
+	private static Greeting save(Greeting greeting) {
+		if(greetingMap == null) {
+			greetingMap = new HashMap<BigInteger, Greeting>();
+			nextId = BigInteger.ONE;
+		}
+		greeting.setId(nextId);
+		nextId = nextId.add(BigInteger.ONE);
+		greetingMap.put(greeting.getId(), greeting);
+		return greeting;
+		
+	}
+
+	@RequestMapping(value = "/api/greetings", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Collection<Greeting>> getGreetings() {
+		
+		Collection<Greeting> greetings = greetingMap.values();
+		
+		return new ResponseEntity<Collection<Greeting>>(greetings, HttpStatus.OK);
+		
+	}
+	
+	@RequestMapping(value = "/api/greetings/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Greeting> getGreeting(@PathVariable("id") BigInteger id) {
+		
+		Greeting greeting = greetingMap.get(id);
+		if(greeting == null) {
+			return new ResponseEntity<Greeting>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Greeting>(greeting, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/api/greetings", method = RequestMethod.POST,
+			consumes=MediaType.APPLICATION_JSON_VALUE, 
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Greeting> createGreeting(@RequestBody Greeting greeting) {
+		
+		Greeting savedGreeting = save(greeting);
+		return new ResponseEntity<Greeting>(savedGreeting, HttpStatus.CREATED);
+		
+	}
+		
+
+}
