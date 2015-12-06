3605b1a319941647eaec16e9e948b15fa6b5296d
Emre Demirel <emre.demirel@btc-ag.com.tr>
2015-12-06 04:08:29 +0200

    meeting controller revised to create a new meeting, request by hasancan

--- bucomp/src/main/java/bucomp/application/web/api/MeetingController.java
+++ bucomp/src/main/java/bucomp/application/web/api/MeetingController.java
@@ -1,6 +1,8 @@
 package bucomp.application.web.api;
 
+import java.util.Date;
 import java.util.Collection;
+import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -9,10 +11,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import bucomp.application.model.Community;
 import bucomp.application.model.Meeting;
+import bucomp.application.model.Meetingattendant;
 import bucomp.application.web.api.dao.CommunityDao;
 import bucomp.application.web.api.dao.CommunityDaoImpl;
 import bucomp.application.web.api.dao.MeetingDao;
@@ -33,15 +37,26 @@ public class MeetingController {
 
 	@RequestMapping(value = "/api/meetings/{communityId}", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Meeting> createMeeting(@PathVariable("communityId") Integer communityId,
-			@RequestBody Meeting meeting) {
+			@RequestParam(value = "userId") Integer userId,
+			@RequestParam(value = "communityIdToSave") Integer communityIdToSave,
+			@RequestParam(value = "startTime") String startTime, @RequestParam(value = "location") String location,
+			@RequestParam(value = "duration") Integer duration,
+			@RequestParam(value = "attendants") List<Meetingattendant> meetingAttendants) {
+		Meeting meeting = new Meeting();
 
+		for (Meetingattendant ma : meetingAttendants) {
+			meeting.addMeetingattendant(ma);
+		}
+		meeting.setCommunity(communityDao.getCommunityById(communityIdToSave));
+		meeting.setDuration(duration);
+		meeting.setLocation(location);
+		meeting.setMeetingDate(new Date());
 		meeting = meetingDao.saveMeeting(meeting);
 		if (meeting == null) {
 			return new ResponseEntity<Meeting>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 
 		return new ResponseEntity<Meeting>(meeting, HttpStatus.CREATED);
-
 	}
 
 	@RequestMapping(value = "api/meetings/{id}", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
