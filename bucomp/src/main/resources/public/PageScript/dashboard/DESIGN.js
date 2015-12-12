12b40322afef7b4b0dfca9962fcdbde4604ef2d6
Emre Gurer <emre.gurer@gmail.com>
2015-12-13 00:50:51 +0200

    Popular tags are updated. Issue #115 is fixed.

--- bucomp/src/main/resources/public/PageScript/dashboard/DESIGN.js
+++ bucomp/src/main/resources/public/PageScript/dashboard/DESIGN.js
@@ -4,20 +4,37 @@
 
 (function () {
 	var DESIGN = { 
-			GetCommunitiesByUserId: function (userId, callback, callback_err) {
-	            try {
-	               SP_BANK.GetCommunitiesByUserId(userId, callback, callback_err);
-	            } catch (err) {
-	                callback_err();
-	            }
-	        },
-            GetUserInfo: function(userId, callback, callback_err){
-                try {
-                       SP_BANK.GetUserInfo(userId, callback, callback_err);
-                    } catch (err) {
-                        callback_err();
-                    }
+        GetCommunitiesByUserId: function (userId, callback, callback_err) {
+            try {
+               SP_BANK.GetCommunitiesByUserId(userId, callback, callback_err);
+            } catch (err) {
+                callback_err();
             }
-	    } 
+        },
+        GetUserInfo: function(userId, callback, callback_err){
+            try {
+                   SP_BANK.GetUserInfo(userId, callback, callback_err);
+                } catch (err) {
+                    callback_err();
+                }
+        },
+        GetPopularTags: function() {
+            SP_BANK.GetPopularTags(DESIGN.FillPopularTags,null);
+        },
+        FillPopularTags: function(data) {
+            if(data != null) {
+                $("#popularTags").html("");
+                for(var i = 0; i < data.length; i++) {
+                    $("#popularTags").append(
+                        $("<li>").append(
+                            $("<a>").attr("dir","ltr").append(data[i].tagName)
+                        ).append(
+                            $("<span>").attr("dir","ltr").append("(" + data[i].tagCount + ")")
+                        )
+                    );
+                }
+            }
+        }
+    } 
     if (!window.DESIGN) { window.DESIGN = DESIGN; }
 })();
--- bucomp/src/main/resources/public/PageScript/dashboard/SP_BANK.js
+++ bucomp/src/main/resources/public/PageScript/dashboard/SP_BANK.js
@@ -2,7 +2,7 @@
 
 (function () {
     var SP_BANK = {
-    		GetCommunitiesByUserId: function (userId, callback, callback_err) {
+        GetCommunitiesByUserId: function (userId, callback, callback_err) {
             try {
                 $.ajax({
                     type: "GET",
@@ -32,8 +32,7 @@
                 }
             }
         },
-        GetUserInfo: function(userId, callback, callback_err)
-        {
+        GetUserInfo: function(userId, callback, callback_err){
             try {
                 $.ajax({
                     type: "GET",
@@ -60,6 +59,36 @@
             catch (err) {
                 if (typeof callback_err == 'function') {
                     callback_err(-2, 'TEST');
+                }
+            }
+        },
+        GetPopularTags: function(callback, callback_err) {
+            try {
+                $.ajax({
+                    type: "GET",
+                    url: GLOBALS.ServiceParameter + "/communities/tagcount",
+                    contentType: "application/json; charset=utf-8",
+                    success: function (msg) {
+                        if (msg == null) {
+                            if (typeof callback == 'function') {
+                                callback(null);
+                            }
+                        }
+                        else{
+                            var _data = eval(msg);
+                            callback(_data);
+                        }
+                    },
+                    error: function (msg) {
+                        if (typeof callback_err == 'function') {
+                            callback_err(msg);
+                        }
+                    }
+                });
+            }
+            catch (err) {
+                if (typeof callback_err == 'function') {
+                    callback_err(err);
                 }
             }
         }
