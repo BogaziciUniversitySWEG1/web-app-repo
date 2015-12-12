b210cf34f234bc7cba60f06df3daee743080ba54
Emre Gurer <emre.gurer@gmail.com>
2015-12-12 20:01:20 +0200

    Join button's functionality is updated. Popular tags are added.

--- bucomp/src/main/resources/public/PageScript/index/DESIGN.js
+++ bucomp/src/main/resources/public/PageScript/index/DESIGN.js
@@ -10,6 +10,60 @@
             } catch (err) {
                 callback_err();
             }
+        },
+        GetPopularTags: function() {
+            SP_BANK.GetPopularTags(DESIGN.FillPopularTags,null);
+        },
+        FillPopularTags: function(data) {
+            if(data != null) {
+                $("#popularTags").html("");
+                for(var i = 0; i < data.length; i++) {
+                    $("#popularTags").append(
+                        $("<li>").append(
+                            $("<a>").attr("dir","ltr").append(data[i].tagName)
+                        ).append(
+                            $("<span>").attr("dir","ltr").append("(" + data[i].tagCount + ")")
+                        )
+                    );
+                }
+            }
+        },
+        JoinCommunity: function(communityId) {
+            var userId = GetQueryStringValue("uid");
+            var roleId = 4;
+            SP_BANK.JoinCommunity(userId, communityId, roleId, DESIGN.JoinSuccess, DESIGN.JoinError);
+        },
+        JoinSuccess: function(userId, communityId) {
+            alert("You became the member of the community.");
+            var url = "community.html?uid=" + userId + "&cid=" + communityId;
+            window.location = url;
+        },
+        JoinError: function(data) {
+        	alert("Alert","An error has been occured. Please contact to community owner.");
+        },
+        ShowRequestModal: function(communityId) {
+            $("#spnCommunityId").html(communityId);
+            $("#divRequest").show();
+            GUI_HELPER.OPENWINDOW("divRequest","Join Request",true, true, false);
+        },
+        SendJoinRequest: function() {
+            var communityId = $("#spnCommunityId").html();
+            var userId = GetQueryStringValue("uid");
+            var explanation = $("#txtExplanation").val();
+            SP_BANK.SendJoinRequest(communityId, userId, explanation, DESIGN.JoinRequestSuccess, DESIGN.JoinRequestError);
+            $("#spnCommunityId").html("");
+        },
+        CancelJoinRequest: function() {
+            $("#divRequest").hide();
+            GUI_HELPER.CLOSEWINDOW("divRequest");
+        },
+        JoinRequestSuccess: function(data, communityId) {
+            alert("Your join request is sent to owner of the community. When the request is approved, you will be a member of the community.");
+            var url = "community.html?cid=" + communityId + "&uid=" + GetQueryStringValue("uid");
+            window.location = url;
+        },
+        JoinRequestError: function(data, communityId) {
+            GUI_HELPER.ALERT("Alert","An error has been occured. Please contact to community owner.",GUI_HELPER.WARNING);
         }
     }
     if (!window.DESIGN) {
--- bucomp/src/main/resources/public/PageScript/index/SP_BANK.js
+++ bucomp/src/main/resources/public/PageScript/index/SP_BANK.js
@@ -29,7 +29,96 @@
                     callback_err(-2, 'TEST');
                 }
             }
+        },
+        GetPopularTags: function(callback, callback_err) {
+            try {
+                $.ajax({
+                    type: "GET",
+                    url: GLOBALS.ServiceParameter + "/communities/tagcount",
+                    contentType: "application/json; charset=utf-8",
+                    success: function (msg) {
+                        if (msg == null) {
+                            if (typeof callback == 'function') {
+                                callback(null);
+                            }
+                        }
+                        else{
+                            var _data = eval(msg);
+                            callback(_data);
+                        }
+                    },
+                    error: function (msg) {
+                        if (typeof callback_err == 'function') {
+                            callback_err(msg);
+                        }
+                    }
+                });
+            }
+            catch (err) {
+                if (typeof callback_err == 'function') {
+                    callback_err(err);
+                }
+            }
+        },
+        JoinCommunity: function (userId, communityId, roleId, callback, callback_err) { 
+            try {
+                var form = new FormData();
+                form.append("communityId", communityId);
+                form.append("userId", userId);
+                form.append("roleId", roleId);
+                
+                var settings = {
+                    "async": true,
+                    "crossDomain": true,
+                    "url": "api/communityMembers",
+                    "method": "POST",
+                    "headers": {
+                        "cache-control": "no-cache",
+                        "postman-token": "38db6183-c7ee-1717-6ce3-e1e485ec518d"
+                    },
+                    "processData": false,
+                    "contentType": false,
+                    "mimeType": "multipart/form-data",
+                    "data": form
+                }
+                
+                $.ajax(settings).done(function (response) {
+                    callback(userId, communityId); 
+                });
+            }
+            catch (err) {
+                callback_err(err);
+            }
+        },
+        SendJoinRequest: function(communityId, userId, explanation, callback, callback_err) {
+            try{
+                var form = new FormData();
+                form.append("communityId", communityId);
+                form.append("userId", userId);
+                form.append("explanation", explanation);
+                
+                var settings = {
+                    "async": true,
+                    "crossDomain": true,
+                    "url": "api/communityRequests",
+                    "method": "POST",
+                    "headers": {
+                        "cache-control": "no-cache",
+                        "postman-token": "38db6183-c7ee-1717-6ce3-e1e485ec518d"
+                    },
+                    "processData": false,
+                    "contentType": false,
+                    "mimeType": "multipart/form-data",
+                    "data": form
+                }
+                
+                $.ajax(settings).done(function (response) {
+                    callback(response); 
+                });
+            } catch(err) {
+                callback_err(err);
+            }
         }
-        }
+    }
     if (!window.SP_BANK) { window.SP_BANK = SP_BANK; }
 })();
