e4b8a9cece327fc3cafb3d275889dd8a41f1c7bd
Alper Karatepe <alperkaratepe@gmail.com>
2015-12-19 16:06:23 +0200

    Documentation added for ScheduledTask

--- bucomp/src/main/java/bucomp/application/jobs/ScheduledTasks.java
+++ bucomp/src/main/java/bucomp/application/jobs/ScheduledTasks.java
@@ -14,28 +14,38 @@ import bucomp.application.web.api.dao.MeetingDaoImpl;
 
 @Component
 public class ScheduledTasks {
-	
+
 	private MeetingDao mdao = new MeetingDaoImpl();
 
-    @Scheduled(fixedRate = 60000)
-    public void updateMeetingStatus() {
-        List<Meeting> meetings = mdao.getAllMeetings(0);
-        Date now = new Date();
-        Date tomorrow = null;
-        for (Iterator<Meeting> iterator = meetings.iterator(); iterator.hasNext();) {
+	/**
+	 * This job runs every minute and checks upcoming meetings, if the meeting
+	 * start time is passed in updates the status as ongoing. Finished meeting
+	 * statuses are updated via user interface by clicking the finish meeting
+	 * button. If it is forgotten, this scheduled job updates finished meeting
+	 * status one day later.
+	 * 
+	 */
+	@Scheduled(fixedRate = 60000)
+	public void updateMeetingStatus() {
+		// get upcomig all meetings (status=0)
+		List<Meeting> meetings = mdao.getAllMeetings(0);
+		Date now = new Date(); // used to check ongoing meetings
+		Date tomorrow = null; // used to check finished meetings
+		for (Iterator<Meeting> iterator = meetings.iterator(); iterator
+				.hasNext();) {
 			Meeting m = iterator.next();
 			now = new Date();
-			if(now.compareTo(m.getStartTime()) > 0) {
+			if (now.compareTo(m.getStartTime()) > 0) {
 				mdao.updateMeetingStatus(m.getMeetingId(), 1);
 			}
-			Calendar c = Calendar.getInstance(); 
-			c.setTime(now); 
+			Calendar c = Calendar.getInstance();
+			c.setTime(now);
 			c.add(Calendar.DATE, 1);
 			tomorrow = c.getTime();
-			if(tomorrow.compareTo(m.getEndTime()) > 0) {
+			if (tomorrow.compareTo(m.getEndTime()) > 0) {
 				mdao.updateMeetingStatus(m.getMeetingId(), 2);
 			}
 		}
-        
-    }
+
+	}
 }
