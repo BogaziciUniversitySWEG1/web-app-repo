ca56bab52ed5087bfa7c8ebea4fde9b90b57500f
Emre Gurer <emre.gurer@gmail.com>
2015-12-19 16:14:18 +0200

    Live meeting is implemented.

--- bucomp/src/main/resources/public/PageScript/livemeeting/DESIGN.js
+++ bucomp/src/main/resources/public/PageScript/livemeeting/DESIGN.js
@@ -1,15 +1,58 @@
-﻿/// <reference path="../CommonScripts/GUI_HELPER.js" />
+/// <reference path="../CommonScripts/GUI_HELPER.js" />
 /// <reference path="SP_BANK.js" />
 /// <reference path="GLOBALS.js" />
 
 (function () {
     var DESIGN = { 
-        test: function () {
-            try {
-               SP_BANK.TEST(paream1, DESIGN.LoginCallback, GUI_HELPER.SERVICE_CALLBACK_ERR);
-            } catch (err) {
-                GUI_HELPER.ALERT('test', err, GUI_HELPER.ERROR);
-            }
+        GetMeetingInfo: function() {
+            GLOBALS.MeetingId = GetQueryStringValue("mid");
+            GLOBALS.CommunityId = GetQueryStringValue("cid");
+            GLOBALS.UserId = GetQueryStringValue("uid");
+            
+            $('#search-toggle-nav').slideToggle();
+            DESIGN.myToggleClass($("#user-toggle"));
+            $('#menu-toggle').removeClass('active');
+            
+            DESIGN.GetAttendants();
+            setInterval(DESIGN.GetContent,1000);
+        },
+        GetAttendants: function() {
+            
+        },
+        FillAttendants: function(data) {
+            
+        },
+        GetContent: function() {
+            SP_BANK.GetContent(DESIGN.FillContent, DESIGN.FillContentError);
+        },
+        FillContent: function(data) {
+            $("#speechAreaField").val(data);
+        },
+        FillContentError: function(data) {
+            
+        },
+        PostContent: function() {
+            SP_BANK.PostContent(DESIGN.PostContentSuccess, DESIGN.PostContentError);
+            $("#commentField").val("");
+        },
+        PostContentSuccess: function(data) {
+            
+        },
+        PostContentError: function(data) {
+            
+        },
+        ReturnToCommunity: function() {
+            var url = "community.html?cid=" + GLOBALS.CommunityId + "&uid=" + GLOBALS.UserId;
+            window.location = url;
+        },
+        myToggleClass: function ($myvar) {
+            if ($myvar.hasClass('active')) {
+                $myvar.removeClass('active');
+            } else {
+                $myvar.addClass('active');
         }
-    } if (!window.DESIGN) { window.DESIGN = DESIGN; }
+    }
+    } 
+    
+    if (!window.DESIGN) { window.DESIGN = DESIGN; }
 })();
--- bucomp/src/main/resources/public/PageScript/livemeeting/SP_BANK.js
+++ bucomp/src/main/resources/public/PageScript/livemeeting/SP_BANK.js
@@ -1,47 +1,60 @@
-﻿(function () {
+(function () {
     var SP_BANK = {
-        TEST: function (param1, callback, callback_err) {
+        GetContent: function(callback, callback_err) {
             try {
                 $.ajax({
-                    type: "POST",
-                    url: ServiceParameter + "/TEST",
-                    data: "{param1:'" + param1 + "'}",
+                    type: "GET",
+                    url: "api/chatroom/getChatHistory?meetingId=" + GLOBALS.MeetingId,
                     contentType: "application/json; charset=utf-8",
-                    dataType: "json",
                     success: function (msg) {
-                        if (msg.d.length == 0 || msg.d == null) {
-                            if (typeof callback == 'function') {
-                                callback(null);
-                            }
-                        }
-                        else if (msg.d <= 0) {
-                            if (typeof callback_err == 'function') {
-                                callback_err(msg.d, 'TEST');
-                            }
+                      	if (msg == null) {
+                            callback(null);
                         }
                         else {
-                            var _data = eval("(" + msg.d + ")");
-                            if (typeof callback_err == 'function' && _data[0] != null && typeof _data[0].ErrorCode != 'undefined') {
-                                callback_err(_data, 'TEST');
-                            }
-                            else if (typeof callback == 'function') {
-                                callback(_data);
-                            }
+                            callback(msg);
                         }
                     },
                     error: function (msg) {
-                        if (typeof callback_err == 'function') {
-                            callback_err(-1, 'TEST');
-                        }
+                        callback_err(msg);
                     }
                 });
             }
             catch (err) {
+                 callback_err(err);
+            }
+        },
+        PostContent: function(callback, callback_err) {
+            try {
+                var form = new FormData();
+                form.append("userId", GLOBALS.UserId);
+                form.append("meetingId", GLOBALS.MeetingId);
+                form.append("message", $("#commentField").val());
+                
+                var settings = {
+                    "async": true,
+                    "crossDomain": true,
+                    "url": "api/chatroom/sendMessage",
+                    "method": "POST",
+                    "headers": {
+                        "cache-control": "no-cache",
+                        "postman-token": "38db6183-c7ee-1717-6ce3-e1e485ec518d"
+                    },
+                    "processData": false,
+                    "contentType": false,
+                    "mimeType": "multipart/form-data",
+                    "data": form
+                }
+                
+                $.ajax(settings).done(function (response) {
+                    callback(response); 
+                });
+            }
+            catch(err) {
                 if (typeof callback_err == 'function') {
-                    callback_err(-2, 'TEST');
+                    callback_err(err);
                 }
             }
-        } 
+        }
     }
     if (!window.SP_BANK) { window.SP_BANK = SP_BANK; }
 })();
