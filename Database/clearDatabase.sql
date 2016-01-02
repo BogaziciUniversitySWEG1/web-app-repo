c9464334652f1796791151bd6ca69462e01ed2eb
Alper Karatepe <alperkaratepe@gmail.com>
2016-01-02 14:32:44 +0200

    Database clear script added.

---
+++ Database/clearDatabase.sql
@@ -0,0 +1,14 @@
+delete from communitymembers
+delete from communityrequests
+delete from meetingattendants
+delete from meetingnotes
+delete from meetingresources
+delete from meeting_invitees
+delete from resources
+delete from tagrelations
+delete from topics
+delete from meetings
+delete from communities
+delete from posts
+delete from users
+delete from tags
