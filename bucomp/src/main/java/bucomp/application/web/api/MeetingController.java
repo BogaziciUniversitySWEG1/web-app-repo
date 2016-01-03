da974ad69b1accf920eadf6ef4d59c2182c9864f
Alper Karatepe <alperkaratepe@gmail.com>
2016-01-03 14:49:58 +0200

    /api/meetings/resources/{meetingId} service added

--- bucomp/src/main/java/bucomp/application/web/api/MeetingController.java
+++ bucomp/src/main/java/bucomp/application/web/api/MeetingController.java
@@ -158,17 +158,6 @@ public class MeetingController {
 	
 	@RequestMapping(value = "/api/meetings/resources/{meetingId}", 
 			method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Collection<Resource>> getCommunityResources(
-			@PathVariable("meetingId") Integer meetingId) {
-		Collection<Resource> resources = rdao.getCommunityResources(meetingId);
-		if (resources == null || resources.size() == 0) {
-			return new ResponseEntity<Collection<Resource>>(resources, HttpStatus.NO_CONTENT);
-		}
-		return new ResponseEntity<Collection<Resource>>(resources, HttpStatus.OK);
-	}
-	
-	@RequestMapping(value = "/api/meetings/resources/{meetingId}", 
-			method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Collection<Resource>> getMeetingResources(
 			@PathVariable("meetingId") Integer meetingId) {
 		Collection<Resource> resources = rdao.getMeetingResources(meetingId);
